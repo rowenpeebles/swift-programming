15da3d88028f4dd47a85c00f428af78a1ceb9999
Rowen Peebles <rowen.peebles@outlook.com>
2022-05-28 00:05:59 +0100

    Adding Swift DataTypes

---
+++ Swift.Introduction/DataTypes/DataTypes.swift
@@ -0,0 +1,27 @@
+// Swift Data Types
+
+// Character
+var letter: Character = "s"
+print(letter)
+
+// String
+var language: String = "swift"
+print(language)
+
+// Integer
+var number: Int = 3
+print(number)
+
+// Boolean
+let passCheck: Bool = true
+print(passCheck)
+let failCheck: Bool = false
+print(failCheck)
+
+// Float
+let piValue: Float = 3.14
+print(piValue)
+
+// Double
+let latitude: Double = 27.7007697012432
+print(latitude)
