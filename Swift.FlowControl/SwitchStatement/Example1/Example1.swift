45d3b00b57afe1a388edf62164fae829f29d652b
Rowen Peebles <rowen.peebles@outlook.com>
2022-05-29 22:18:17 +0100

    Adding Swift SwitchStatement

---
+++ Swift.FlowControl/SwitchStatement/Example1/Example1.swift
@@ -0,0 +1,31 @@
+// Simple program using Switch Statement
+
+let dayOfWeek = 4
+
+switch dayOfWeek {
+
+    case 1:
+        print("Saturday")
+    
+    case 2:
+        print("Sunday")
+    
+    case 3:
+        print("Monday")
+    
+    case 4: 
+        print("Tuesday")
+    
+    case 5:
+        print("Wednesday")
+    
+    case 6:
+        print("Thursday")
+    
+    case 7:
+        print("Friday")
+
+    default:
+        print("Invalid Day")
+
+}
---
+++ Swift.FlowControl/SwitchStatement/Example2/Example2.swift
@@ -0,0 +1,19 @@
+// Switch Statement with a range
+
+let ageGroup = 33
+
+switch ageGroup {
+
+    case 0...16:
+        print("Child")
+
+    case 17...30:
+        print("Young Adults")
+
+    case 31...45:
+        print("Middle Aged")
+
+    default:
+        print("Old-Aged Adults")
+
+}
