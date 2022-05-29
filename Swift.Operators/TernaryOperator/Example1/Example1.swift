8033eb643ccc684b7fa39dbb4ce79c9e9b736d87
Rowen Peebles <rowen.peebles@outlook.com>
2022-05-29 21:41:25 +0100

    Adding Swift Ternary Operator

---
+++ Swift.Operators/TernaryOperator/Example1/Example1.swift
@@ -0,0 +1,8 @@
+// Ternary Operator
+
+let marks = 60
+
+// Use of Ternary operator
+let result = (marks >= 40) ? "pass" : "fail"
+
+print("You " + result + " the exam")
---
+++ Swift.Operators/TernaryOperator/Example2/Example2.swift
@@ -0,0 +1,7 @@
+// Nested Ternary Operator
+
+let num = 7
+
+let result = (num == 0) ? "Zero" : ((num > 0) ? "Positive" : "Negative")
+
+print("The number is \(result).")
